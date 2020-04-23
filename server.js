927e508b695dc3472975f8549df65969e87e31e4
Wade-Coplen <wadecoplen@gmail.com>
2020-04-22 19:34:50 -0700

    3. server finishd, authrouter started

--- server.js
+++ server.js
@@ -1,9 +1,50 @@
+const session = require('express-session')
+const knexSessionStore = require('conenect-session-knex')('session')
+const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
+
+const restrict = require('./auth/restricted-middleware')
+
+//pull in our express routers
+const usersRouter = require('./users/users-router')
+const authRouter = require('./auth/auth-router')
+
+//create the server object
 const server = express()
 
-server.use(helmet());
+//create a config object for express session
+const sessionConfig = {
+    name: '',
+    secret: '',
+    cookie: {
+        maxAge: 3600 * 1000,
+        secure: false,
+        httpOnly: true
+    },
+    resave: false,
+    saveUninitialized: false,
 
-server.use(cors())
+    store: new KnexSessionStore (
+        {
+            knex: require('./db.config'),
+            tablename: 'sessions',
+            sidfieldname: 'sid',
+            createtable: true,
+            clearInterval: 3600 * 1000
+        }
+    )
+}
+//global middleware
+server.use(helmet());
 server.use(express.json);
-server.use('/TABLE', someRouter)
+server.use(cors())
+server.use(session(sessionConfig))
+server.use('/users', restricted, usersRouter)
+server.use('/auth', authRouter)
+
+server.get('/', (req, res) => {
+    res.json({api: 'up'})
+})
 
 module.exports = server;
